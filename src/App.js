e8c4991e28108c60071b02bc6ed730bd3823d9c3
JwaSangHeon <whk4051@naver.com>
2022-11-15 15:50:25 +0900

    useRef를 통한 input value 접근, useNavigate를 통한 경로 수정, 단어추가, 날짜 추가 로직 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import DayList from "./components/DayList";
 import Header from "./components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
+import CreateDay from "./components/CreateDay";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
           <Route path="*" element={<EmptyPage />} />
         </Routes>
       </div>
